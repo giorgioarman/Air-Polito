aac678710e9f6791051ca75ba09b5dd915158b07
Arman <arman_ebrahimi@hotmail.com>
2020-01-14 19:30:06 +0100

    first commit

---
+++ Server/Resource_Catalog/ResourceCatalog.py
@@ -0,0 +1,46 @@
+import cherrypy
+import json
+
+# Creation of the class 'ResourceCatalog'
+class ResourceCatalog(object): 
+    exposed = True
+    # Definzione funzione GET
+    def GET(self, *uri, **param):
+        with open("RcData.json", "r") as t: 
+            tmpData = t.read()
+            RcDataJson = json.loads(tmpData) #Reading and storage of datas from file 'RcData.json'
+
+        if len(uri) == 0:
+            return "you have not entered the command"
+        else:
+            RequestCommand = str(uri[0]).lower() #Managing of the request from other devices 
+            if RequestCommand in RcDataJson: 
+                res = RcDataJson[RequestCommand]
+                reqData = json.dumps(res)
+                return reqData
+
+
+if '__main__' == __name__:
+
+    with open("RcConfig.json", "r") as f:
+        tmpConf = f.read()
+        ResourceCatalogConfig = json.loads(tmpConf)
+
+    RcIp = ResourceCatalogConfig["ResourseCatalogInfo"]["ip"]
+    RcPort = ResourceCatalogConfig["ResourseCatalogInfo"]["port"]
+
+    confCherry = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+        }
+    }
+
+    cherrypy.tree.mount(ResourceCatalog(), '/', confCherry)
+    cherrypy.config.update({
+        "server.socket_host": str(RcIp),
+        "server.socket_port": int(RcPort),
+    })
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
